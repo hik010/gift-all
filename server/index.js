507c49b23421889f3831e195155c224af60f0a17
Hyo Kim <hyo.kim968@gmail.com>
2021-12-14 09:52:09 -0800

    user db

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ const app = express();
 const path = require('path');
 const redis = require('redis');
 const socketio = require('socket.io');
+const seed = require('../script/seed');
 
 const router = require('./api');
+const db = require('./db');
 const redisClient = redis.createClient();
 
 if (process.env.NODE_ENV !== 'production') require('../secrets');
@@ -18,12 +20,11 @@ app.use(express.urlencoded({ extended: true }));
 // static middleware
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+});
 
-app.get('/', (req,res,next) => {
-  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
-})
-
-app.use('/api',require('./api'))
+app.use('/api', require('./api'));
 
 app.get('/maps-api-key', (req, res) => {
   res.send(process.env.MAPS_API_KEY);
@@ -43,11 +44,21 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 8090;
 
 const init = async () => {
-  const server = app.listen(PORT, () => {
-    console.log(`Serving locally on port ${PORT}`);
-  });
-  const io = socketio(server);
-  require('./socket')(io); //server socket events
+  try {
+    if (process.env.SEED === 'true') {
+      await seed();
+    } else {
+      await db.sync();
+    }
+    // start listening (and create a 'server' object representing our server)
+    const server = app.listen(PORT, () =>
+      console.log(`Mixing it up on port ${PORT}`)
+    );
+    const io = socketio(server);
+    require('./socket')(io); //server socket events
+  } catch (ex) {
+    console.log(ex);
+  }
 };
 
 init();
