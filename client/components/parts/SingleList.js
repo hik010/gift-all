762b8da64d6bf666e8dd8ba32fa52b75819da9ae
Hyo Kim <hyo.kim968@gmail.com>
2022-01-25 16:44:08 -0800

    responsiveness for date tags and button group

--- client/components/parts/SingleList.js
+++ client/components/parts/SingleList.js
@@ -3,14 +3,18 @@ import { useSelector } from 'react-redux';
 import SingleItem from './SingleItem';
 
 const SingleList = ({ list, clickCard }) => {
+  const singleList = useSelector((state) => state.singleList);
   return (
     <div
-      className="list-card position-relative"
+      className="list-card position-relative d-"
       key={list.id}
       onClick={(event) => clickCard(event, list)}
+      onMouseOver={(event) => {
+        console.log(event.target);
+      }}
     >
-      <div id="top-bar">
-        <h4 className="d-inline" id='item-name'>
+      <div id="top-bar" className='d-flex flex-wrap'>
+        <h4 className="d-inline me-2" id="item-name">
           {list.name}
         </h4>
         {list.date && (
@@ -18,26 +22,15 @@ const SingleList = ({ list, clickCard }) => {
         )}
       </div>
 
-      <ListContent list={list} />
+      {/* only show the content if selected list */}
+      {singleList.id === list.id && <ListContent list={list} />}
     </div>
   );
 };
 
 const ListContent = ({ list }) => {
-  const singleList = useSelector((state) => state.singleList);
-  if (singleList.id !== list.id) return '';
-
   return (
     <>
-      {singleList.items.length !== 0 ? (
-        <div className="list-items mt-3 d-flex flex-wrap justify-content-start">
-          {singleList.items.map((item) => (
-            <SingleItem key={item.id} item={item} />
-          ))}
-        </div>
-      ) : (
-        <div className="mt-2"> No items here. </div>
-      )}
       <div className="buttons-group">
         <button
           className="btn bg-success"
@@ -56,6 +49,15 @@ const ListContent = ({ list }) => {
           Delete
         </button>
       </div>
+      {list.items.length !== 0 ? (
+        <div className="list-items mt-3 d-flex flex-wrap justify-content-start">
+          {list.items.map((item) => (
+            <SingleItem key={item.id} item={item} />
+          ))}
+        </div>
+      ) : (
+        <div className="mt-3"> No items here. </div>
+      )}
     </>
   );
 };
