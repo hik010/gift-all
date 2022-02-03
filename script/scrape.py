18bf84c8ed41608c431bc3279e4ea07bde3193e1
Hyo Kim <hyo.kim968@gmail.com>
2022-02-03 02:36:33 -0800

    header changed

--- script/scrape.py
+++ script/scrape.py
@@ -6,16 +6,12 @@ import re
 
 header = {
   "User-Agent":
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582",
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36",
   "Accept-Language": "en-US,en;q=0.9,ko;q=0.8,zh-CN;q=0.7,zh;q=0.6"
   }
 
 def amazon_scrape(amazon_link):
-  header = {
-    "User-Agent":
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582",
-    "Accept-Language": "en-US,en;q=0.9,ko;q=0.8,zh-CN;q=0.7,zh;q=0.6"
-    }
+
 
   response = requests.get(amazon_link, headers=header)
   soup = BeautifulSoup(response.text, "lxml")
